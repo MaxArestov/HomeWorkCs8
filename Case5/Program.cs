971387ed3bd934fb7e13f7a668598fdced6af078
Maxim Arestov <aresmax1993@gmail.com>
2022-11-21 10:50:28 +0300

    Done

---
+++ Case5/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 62. Напишите метод, который заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+Console.Clear();
+Console.WriteLine("Введите количество строк и столбцов массива:");
+int rowsAndColumns = GetNumber();
+
+
+int[,] arrayNums = new int[rowsAndColumns, rowsAndColumns];
+FillArraySpiral(arrayNums, rowsAndColumns);
+Print2dArray(arrayNums);
+Console.WriteLine();
+
+
+void FillArraySpiral(int[,] array, int n)
+{
+    int i = 0, j = 0;
+    int value = 1;
+    for (int e = 0; e < n * n; e++)
+    {
+        int k = 0;
+        do
+        {
+            array[i, j++] = value++; 
+        } 
+        while (++k < n - 1);
+        for (k = 0; k < n - 1; k++) array[i++, j] = value++;
+        for (k = 0; k < n - 1; k++) array[i, j--] = value++;
+        for (k = 0; k < n - 1; k++) array[i--, j] = value++;
+        ++i; ++j;
+        n = n < 2 ? 0 : n - 2;
+    }
+}
+
+void Print2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int GetNumber()
+{
+    int n = 0;
+    bool check = true;
+    while (check)
+    {
+        bool isParsed = int.TryParse(Console.ReadLine(), out n);
+        if (!isParsed)
+        {
+            Console.WriteLine("Введены некорректные данные. Введите целое число:");
+        }
+        else check = false;
+    }
+    return n;
+}
